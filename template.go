7f14d59eff1509ebdea3214617c04766b246ca78
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-02-22 17:58:04 +0100

    feat: add MAC, LocalMAC and GlobalMAC functions

--- template.go
+++
@@ -1,6 +0,0 @@
-package template
-
-// GenerateRandomNumber generates a random number using the full range of the randomizer.
-func (tr *Randomizer) GenerateRandomNumber() int {
-	return tr.Rand.Int()
-}
